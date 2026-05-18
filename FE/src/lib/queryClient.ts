004da9e3d0820b5da969a48f2d7083bafdd4b7bb
ahhmedsamii77 <ahhmddsamii77@gmail.com>
2026-05-19 01:52:32 +0300

    final-10

--- FE/src/lib/queryClient.ts
+++ FE/src/lib/queryClient.ts
@@ -1,3 +1,11 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false, // Prevent refetch when window regains focus
+            staleTime: 24 * 60 * 60 * 1000, // 1d
+            retry: 1, // Only retry once on failure
+        },
+    },
+});
