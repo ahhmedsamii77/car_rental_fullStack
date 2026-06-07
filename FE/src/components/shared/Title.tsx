ea1b06cfc00b66854c43bd4d0a80863be67fd24f
ahhmedsamii77 <ahhmddsamii77@gmail.com>
2026-06-07 16:21:19 +0300

    p-new2

--- FE/src/components/shared/Title.tsx
+++ FE/src/components/shared/Title.tsx
@@ -6,7 +6,7 @@ export default function Title({ title, description }: { title: string; descripti
       initial={{ y: -30, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5, delay: 0.1 }}
-      className="space-y-3"
+      className="space-y-3 text-center"
     >
       <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl font-[family-name:var(--font-display)] tracking-tight">
         {title}
